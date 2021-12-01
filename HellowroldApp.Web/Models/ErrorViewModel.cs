1ea186ccd73eedeff268dc5b2a079251ce1f0dd3
Muhammad Umair <umair@photogauge.com>
2021-12-01 17:51:43 +0500

    Changing .Net Version 6 to 5

--- HellowroldApp.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace HellowroldApp.Web.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
